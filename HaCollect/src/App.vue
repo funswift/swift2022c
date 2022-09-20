5d2e2af2fd40ee085441518c08b6961fa977a1b4
harueight <public8610@gmail.com>
2022-09-20 18:08:19 +0900

    ノードモジュールの追加 コードの整形 謎のdivタグの追加

--- HaCollect/src/App.vue
+++ HaCollect/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+};
 </script>
