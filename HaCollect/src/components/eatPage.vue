638b81559890c2c110fb357c8aeeea1e3abdfcab
midKitchen <b1020242@fun.ac.jp>
2022-09-22 23:46:58 +0900

    wip: ヘッダー制作とルーター実装

---
+++ HaCollect/src/components/eatPage.vue
@@ -0,0 +1,174 @@
+<template>
+    <div class="header-inner2">
+        <nav class="header-nav">
+            <ul class="header-navList">
+                <li>
+                    <a>
+                        <router-link to="/">ホーム</router-link>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <router-link class="nowPage" to="/eat">ごはん</router-link>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <router-link to="/event">イベント</router-link>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <router-link to="/tour">観光</router-link>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <router-link to="/news">災害</router-link>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="hello">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>これは「ごはん」カテゴリーのページです。</h1>
+        <p>
+            For a guide and recipes on how to configure / customize this project,<br>
+            check out the
+            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+        </p>
+        <h3>Installed CLI Plugins</h3>
+        <ul>
+            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+                    rel="noopener">babel</a></li>
+            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+                    rel="noopener">eslint</a></li>
+        </ul>
+        <h3>Essential Links</h3>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+        </ul>
+        <h3>Ecosystem</h3>
+        <ul>
+            <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+            <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+            <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank"
+                    rel="noopener">vue-devtools</a>
+            </li>
+            <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+        </ul>
+        <h3>Installed CLI Plugins</h3>
+        <ul>
+            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+                    rel="noopener">babel</a></li>
+            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+                    rel="noopener">eslint</a></li>
+        </ul>
+        <h3>Essential Links</h3>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+        </ul>
+        <h3>Ecosystem</h3>
+        <ul>
+            <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+            <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+            <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank"
+                    rel="noopener">vue-devtools</a>
+            </li>
+            <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+
+.header-inner2 {
+    background-color: #f5af5a;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin: 0 auto;
+    position: fixed;
+}
+
+.header-nav {
+    padding-left: 36px;
+    overflow-x: auto;
+    white-space: nowrap;
+}
+
+.header-navList {
+    display: flex;
+    align-items: center;
+}
+
+.header-navList li {
+    margin: 0 20px
+        /* ナビゲーションに左右のスペースを付ける */
+}
+
+.header-navList li a {
+    display: block;
+    /* 扱いやすいようにblock要素にする */
+    font-size: 20px;
+    /* 任意のフォントサイズにする */
+    font-weight: bold;
+    /* 太字にする */
+    color: #fff;
+    text-decoration-line: none;
+    padding: 0 15px;
+}
+
+.header-navList .nowPage {
+    color: #4c9eeb;
+    text-decoration-line: underline;
+}
+
+.header-navList li a:hover {
+    opacity: 0.8;
+    /* ホバーしたときに少し薄くなるようにアニメーションを付ける */
+}
+
+.hello {
+    padding-top: 80px;
+}
+</style>
