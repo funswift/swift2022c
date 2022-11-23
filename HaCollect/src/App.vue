375359a756d304b2912fa2c3b03c20c7ff601691
midKitchen <b1020242@fun.ac.jp>
2022-11-23 18:34:19 +0900

    add: コンフリクト解消時のエラー修正

--- HaCollect/src/App.vue
+++ HaCollect/src/App.vue
@@ -53,13 +53,57 @@
 export default {
     data() {
         return {
-            search_text: '',    //検索バーに打ち込んだのをリアルタイムに格納
+            search_text: '',  //実際にsearchResultコンポーネントに渡すもの
             buttonActive: false,//ボタンを非表示にしておく
             scroll: 0
         }
     },
+    methods: {
+        InitializeData() {
+            this.$store.dispatch('InitializeFireData').then( () => {
+                // this.$store.dispatch('getCategoryData')  
+                this.$store.dispatch('getTopData')
+                this.$store.dispatch('getFoodData')
+                this.$store.dispatch('getNewsData')
+                this.$store.dispatch('getSpaData')
+                this.$store.dispatch('getTourData')
+            }) 
+        },
+        doSearch() {
+            try{
+                // this.$router.push('/searchResult') <-これはただ、リンク変えるだけ
+                this.$router.push({ name: 'searchResult', params: { search: this.search_text } }) //<- これはリンク先に検索したのを渡すことができる
+                this.$store.dispatch('getSearchData', this.search_text)    //検索結果ページで使うデータ(search_fire_data)をstoreから参照 -> searchResult.vueのcomputedのfire_dataに反映
+                this.search_text = '' //検索バーに検索した文字を残さないための処理
+            } catch(e) {
+                console.log('error')
+                //エラーは無視（search_textに何も入力してない時怒られるから書いた）
+            }
+        },
+        deleteText() {
+            this.search_text = '' //検索バーに検索した文字を残さないための処理
+        },
+        // ページのトップに移動する関数
+        scrollTop() {
+            window.scrollTo({
+                top: 0,  //どこに移動するか
+                behavior: 'smooth'  //ここでどのように移動するか決める smoothでゆっくり移動する
+            });
+        },
+        // ボタンが現れる関数
+        scrollWindow() {
+            const top = 100 //topから100pxスクロールしたらボタン登場
+            this.scroll = window.scrollY //垂直方向
+            if (top <= this.scroll) {
+                this.buttonActive = true   //ボタン見えるようになる
+            } else {
+                this.buttonActive = false  //ボタン見えない
+            }
+        }
+    },
     mounted() {
         window.addEventListener('scroll', this.scrollWindow)   //スクロールすると関数が察知してくれる
+        this.InitializeData();
         document.getElementById('button11').addEventListener("click", function (event) {
             document.getElementById('button12').click();
         });
@@ -91,54 +135,6 @@ export default {
             document.getElementById('button51').click();
         });
     },
-    methods: {
-        InitializeData() {
-            this.$store.dispatch('InitializeFireData').then( () => {
-                // this.$store.dispatch('getCategoryData')  
-                this.$store.dispatch('getTopData')
-                this.$store.dispatch('getFoodData')
-                this.$store.dispatch('getNewsData')
-                this.$store.dispatch('getSpaData')
-                this.$store.dispatch('getTourData')
-            }) 
-        },
-        doSearch() {
-            try{
-                // this.$router.push('/searchResult') <-これはただ、リンク変えるだけ
-                this.$router.push({ name: 'searchResult', params: { search: this.search_text } }) //<- これはリンク先に検索したのを渡すことができる
-                this.$store.dispatch('getSearchData', this.search_text)    //検索結果ページで使うデータ(search_fire_data)をstoreから参照 -> searchResult.vueのcomputedのfire_dataに反映
-                this.search_text = '' //検索バーに検索した文字を残さないための処理
-            } catch(e) {
-                console.log('error')
-                //エラーは無視（search_textに何も入力してない時怒られるから書いた）
-            }
-
-        },
-        deleteText() {
-            this.search_text = '' //検索バーに検索した文字を残さないための処理
-        }, 
-        // ページのトップに移動する関数
-        scrollTop() {
-            window.scrollTo({
-                top: 0,  //どこに移動するか
-                behavior: 'smooth'  //ここでどのように移動するか決める smoothでゆっくり移動する
-            });
-        },
-        // ボタンが現れる関数
-        scrollWindow() {
-            const top = 100 //topから100pxスクロールしたらボタン登場
-            this.scroll = window.scrollY //垂直方向
-            if (top <= this.scroll) {
-                this.buttonActive = true   //ボタン見えるようになる
-            } else {
-                this.buttonActive = false  //ボタン見えない
-            }
-        }
-    },
-    mounted() {
-        window.addEventListener('scroll', this.scrollWindow)   //スクロールすると関数が察知してくれる
-        this.InitializeData();
-    },
 }
 </script>
 
@@ -160,7 +156,6 @@ header {
     /* 固定した要素は浮いた状態になるので、横幅100%にする */
     height: 70px;
 }
-
 .header-inner {
     display: flex;
     height: 70px;
@@ -169,7 +164,6 @@ header {
     padding: 0 36px;
     margin: 0 auto;
 }
-
 .header-logoImg {
     display: block;
     /* 扱いやすいようにblock要素にする */
@@ -177,7 +171,6 @@ header {
     /* 横幅を任意の大きさに調整 */
     margin-top: 10px;
 }
-
 .searchArea {
     width: 350px;
     /* inputの幅           */
@@ -211,13 +204,11 @@ header {
     font-style: normal;
     text-align: center;
 }
-
 /*テキスト入力欄にフォーカスか来たとき*/
 .searchArea:focus {
     background-color: #e6f2ff;
     /* フォーカス時背景色  */
 }
-
 .header-inner2 {
     background-color: #ffffee;
     display: flex;
@@ -229,14 +220,12 @@ header {
     height: 60px;
     z-index: 1;
 }
-
 .header-nav {
     display: flex;
     padding: 0 36px;
     overflow-x: auto;
     white-space: nowrap;
 }
-
 .header-nav label {
     background: #FFFFFF;
     border: 1px solid #CCCCCC;
@@ -247,7 +236,6 @@ header {
     margin: 0 3px;
     /* ナビゲーションに左右のスペースを付ける */
 }
-
 .pageChange {
     display: block;
     /* 扱いやすいようにblock要素にする */
@@ -259,20 +247,16 @@ header {
     text-decoration-line: none;
     padding: 5px 15px;
 }
-
 .header-nav input:checked+ label {
     background: rgba(76, 197, 235, 0.25);
     border: 1px solid rgba(76, 197, 235, 0.25);
 }
-
 .header-nav input:checked+ label .pageChange {
     color: #4C79EB;
 }
-
 .header-nav input {
     display: none;
 }
-
 /* ページトップへ自動スクロールするボタンのスタイル */
 .button {
     position: fixed;
@@ -290,138 +274,56 @@ header {
     opacity: 0.7;
     /* ボタンにカーソルを合わせるとポインターになる */
 }
-
 /* アニメーション中のスタイル */
 .button-enter-active,
 .button-leave-active {
     transition: opacity 0.5s;
     /* 何秒かけて変わるのか */
 }
-
 /* 表示するアニメーション */
 .button-enter-from {
     opacity: 0;
 }
-
 .button-enter-to {
     opacity: 0.7;
 }
-
 /* 非表示にするアニメーション */
 .button-leave-from {
     opacity: 0.7;
 }
-
 .button-leave-to {
     opacity: 0;
 }
-
-.header-inner2 {
-    background-color: #ffffee;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    margin: 0 auto;
-    position: fixed;
-    height: 60px;
-    z-index: 1;
-}
-
-.header-nav {
-    padding-left: 36px;
-    overflow-x: auto;
-    white-space: nowrap;
-}
-
-.header-navList {
-    display: flex;
-    align-items: center;
-    margin: 0 17px;
-}
-
-.header-navList li {
-    margin: 0 3px
-        /* ナビゲーションに左右のスペースを付ける */
-}
-
-.header-navList li a {
-    display: block;
-    /* 扱いやすいようにblock要素にする */
-    font-size: 20px;
-    /* 任意のフォントサイズにする */
-    font-weight: bold;
-    /* 太字にする */
-    color: #000;
-    text-decoration-line: none;
-    padding: 5px 15px;
-    background: #FFFFFF;
-    border: 1px solid #CCCCCC;
-    border-radius: 5px;
-    font-family: 'Inter';
-    font-style: normal;
-    width: 82px;
-}
-
-.header-navList .nowPage {
-    color: #4C79EB;
-    background: rgba(76, 197, 235, 0.25);
-    border: 1px solid rgba(76, 197, 235, 0.25);
-}
-
-.header-navList li .searchWord {
-    background: none;
-    border: none;
-}
-
-.header-navList li a:hover {
-    opacity: 0.8;
-    /* ホバーしたときに少し薄くなるようにアニメーションを付ける */
-}
-
-
 /* タブレット・スマートフォン用スタイル */
 @media(max-width: 971px) {
-    .header-nav {
-        padding-left: 0;
-    }
-
     /* .header-logoImg {
         width: 150px;
         margin-top: 10px;
     } */
-
     /* .searchArea {
     width: 225px;
     height: 30px;
     } */
 }
-
-
 /* スマートフォン用スタイル */
 @media(max-width: 647px) {
     .header-inner {
         padding: 0 5px;
     }
-
     .searchArea {
         width: 225px;
         height: 30px;
     }
-
     /* .header-inner {
         display: block;
         height: 35px;
     }
-
     .header-logoImg {
         margin: 0 auto;
     }
-
     .searchArea {
         display: none;
     } */
-
     .header-inner-smart {
         display: flex;
         height: 35px;
@@ -430,12 +332,10 @@ header {
         padding: 0 20px;
         margin: 0 auto;
     }
-
     .cancel {
         font-size: 250%;
         font-weight: bold;
     }
-
     .searchAreaSmart {
         width: 350px;
         /* inputの幅           */
@@ -465,7 +365,6 @@ header {
         outline: 0;
         /* 入力薄枠を非表示    */
     }
-
     /*テキスト入力欄にフォーカスか来たとき*/
     .searchAreaSmart:focus {
         background-color: #e6f2ff;
